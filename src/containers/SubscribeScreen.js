7baaecc184a1dd345c7e8eac9cb5b217f8870d0b
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-07-20 22:54:19 +0200

    20/7

--- src/containers/SubscribeScreen.js
+++ src/containers/SubscribeScreen.js
@@ -120,14 +120,14 @@ class SubscribeScreen extends PureComponent {
     }
 
     subscribeForm(){
-        // if(this.state.idBack && this.state.idFront && this.state.licenceBack && this.state.licenceFront && this.state.nid && this.state.licenseID) {
-        // console.log(this.props.navigation.state.params.cities)
+        if(this.state.idBack && this.state.idFront && this.state.licenceBack && this.state.licenceFront && this.state.nid && this.state.licenseID) {
+        console.log(this.props.navigation.state.params.cities)
             this.props.navigation.navigate("MembershipAddress", {...this.state, membership: this.props.navigation.state.params.membership, states: this.props.navigation.state.params.states, cities: this.props.navigation.state.params.cities})
-        // }else{
-        //     setTimeout(() => {
-        //         Toast.show(__("All Fields are required", this.props.language), Toast.LONG)
-        //     }, 100)
-        // }
+        }else{
+            setTimeout(() => {
+                Toast.show(__("All Fields are required", this.props.language), Toast.LONG)
+            }, 100)
+        }
     }
     render() {
         return (
